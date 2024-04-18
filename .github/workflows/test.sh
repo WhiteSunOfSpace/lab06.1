e2ad2e79fb1b620a18bf115189a1e2b529df198f
WhiteSunOfSpace <113097854+WhiteSunOfSpace@users.noreply.github.com>
2024-04-18 22:40:47 +0300

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,9 +1,9 @@
-sudo chmod +x test.sh
-cmake --build ./formatter_lib
-cmake --build ./formatter_ex_lib 
-cmake --build ./hello_world_application 
-cmake --build ./solver_lib
-cmake --build ./solver_application
+root=./
+cmake --build $root/formatter_lib
+cmake --build $root/formatter_ex_lib 
+cmake --build $root/hello_world_application 
+cmake --build $root/solver_lib
+cmake --build $root/solver_application
 
-./hello_world_application/_build/main
-echo -e '1\n\2\n3' | ./solver_application/_build/main
+$root/hello_world_application/_build/main
+echo -e '1\n\2\n3' | $root/solver_application/_build/main
